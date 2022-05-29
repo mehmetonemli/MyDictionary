51984e5ee7fd39aeea9968c820393ea81331835a
mehmet <m.onemli01@gmail.com>
2022-05-29 20:08:16 +0300

    mydictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    public class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+
+        }
+
+        public void Add(K key,V value)
+        {
+            K[] tempkey = keys;
+            keys = new K[keys.Length + 1];
+            for (int i = 0; i < tempkey.Length; i++)
+            {
+                keys[i] = tempkey[i];
+            }
+            keys[keys.Length - 1] = key;
+
+
+            V[] tempvalue = values;
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempvalue.Length; i++)
+            {
+                values[i] = tempvalue[i];
+            }
+            values[values.Length - 1] = value;
+
+
+        }
+
+        public K[] Key { get { return keys; } }
+
+        public V[] Value { get { return values; } }
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> dictionary = new MyDictionary<int, string>();
+            dictionary.Add(1, "Aydın");
+            dictionary.Add(2, "Ankara");
+            dictionary.Add(3, "İstanbul");
+            dictionary.Add(4, "Bursa");
+            dictionary.Add(5, "İzmir");
+
+            for (int i = 0; i < dictionary.Key.Length; i++)
+            {
+                Console.WriteLine(dictionary.Key[i]);
+            }
+
+            for (int i = 0; i < dictionary.Value.Length; i++)
+            {
+                Console.WriteLine(dictionary.Value[i]);
+            }
+        }
+    }
+}
